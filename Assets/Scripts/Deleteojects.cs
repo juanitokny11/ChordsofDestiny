a1953b288885e7ad8807ae4f71e2a63db0ccffe1
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-10-02 12:59:17 +0200

    eliminar las notas al cogerlas funcionando

---
+++ Assets/Scripts/Deleteojects.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Deleteojects: MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Explode();
+        }
+    }
+	
+	void Explode() {
+		
+		Destroy(gameObject);
+	}
+}
