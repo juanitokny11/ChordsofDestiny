6a69cf9a071326c7921f3dec42b99dff2914ce46
ismael1034 <isma1034@gmail.com>
2018-10-22 10:22:48 +0200

    Menu de credtios

---
+++ Assets/Scripts/Menus/CreditsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour {
+
+    // Use this for initialization
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Credits");
+        Cursor.visible = true;
+    }
+
+    public void Title()
+    {
+        SceneManager.LoadScene("MainM");
+        Cursor.visible = true;
+    }
+
+}
