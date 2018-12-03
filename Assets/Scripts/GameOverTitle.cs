4a66166bf2aebdb88e7bf2e22aafa4dd91e9af8e
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-12-03 17:05:38 +0100

    scena de victoria creada y titulo de gameover variable

---
+++ Assets/Scripts/GameOverTitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverTitle : MonoBehaviour {
+
+	public GameObject frase1;
+	public GameObject frase2;
+	public GameObject frase3;
+	private int states=0;
+	void Start () {
+		states=Random.Range(0,2);
+		if(states==0){
+			frase1.SetActive(true);
+			frase2.SetActive(false);
+			frase3.SetActive(false);
+		}else if (states==1){
+			frase1.SetActive(false);
+			frase2.SetActive(true);
+			frase3.SetActive(false);
+		}else if(states==2){
+			frase1.SetActive(false);
+			frase2.SetActive(false);
+			frase3.SetActive(true);
+		}
+	}
+}
