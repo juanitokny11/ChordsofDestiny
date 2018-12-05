6e8ff6d7c1e72ed4e26b5e16aa01e15b1964007e
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-12-05 20:01:03 +0100

    Revert "añado coliders a la scena"
    This reverts commit a13e104103eb6f83fea9b27fb4ca699b611de05e.

--- Assets/Scripts/DeleteObjects.cs
+++ Assets/Scripts/DeleteObjects.cs
@@ -3,19 +3,12 @@ using System.Collections;
 
 public class DeleteObjects: MonoBehaviour {
 
-    public int life;
-    private void Start()
-    {
-        life = 3;
-    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "ligero" || other.tag == "pesado")
+        if (other.tag == "Player")
         {
-            life--;
-            if (life <= 0){
-                Explode();
-            }
+            Explode();
         }
     }
     
