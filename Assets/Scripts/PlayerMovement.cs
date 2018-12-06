627f065d25fc13b9c982178dccf28a53901ef1bb
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-12-06 16:11:23 +0100

    botonde opciones del menu funcionanado

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -103,7 +103,6 @@ public class PlayerMovement : MonoBehaviour {
     void ParticlePlay(){
         if (jump == true) { 
         particle.Play();
-        Debug.Log("pasa");
         sound.Play(2,1);
         jump = false;
         }
