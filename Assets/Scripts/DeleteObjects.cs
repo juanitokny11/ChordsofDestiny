fae9e773a0169e3c1944bd50f30aca3f18d14cf5
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-12-07 10:51:08 +0100

    objetos sueltan notas aleatorias

--- Assets/Scripts/DeleteObjects.cs
+++ Assets/Scripts/DeleteObjects.cs
@@ -7,18 +7,21 @@ public class DeleteObjects: MonoBehaviour {
     public GameObject[] notas;
     public int random;
     private Vector3 inipos;
+    public EnemyCache[] enemies;
+    public bool spawn=false;
 
     private void Start()
     {
         //notas = new GameObject[4];
         inipos = transform.position;
+        enemies= new EnemyCache[4];
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ligero" || other.tag =="pesado")
         {
             vida--;
-            if (vida <= 0){ 
+            if (vida == 0){ 
             Explode();
             }
         }
@@ -26,12 +29,25 @@ public class DeleteObjects: MonoBehaviour {
     private void Update()
     {
         random = Random.Range(0, notas.Length);
+        if (spawn == true) { 
+        for (int i = 0; i < 1; i++)
+        {
+            enemies[i] = new EnemyCache(notas[random], inipos, transform, Random.Range(1, 3));
+            spawn = false;
+        }
+        }
+
     }
 
     void Explode() {
 
-        Instantiate(notas[random], inipos,Quaternion.identity,transform);
-        Destroy(gameObject);
+        spawn = true;
+        //Instantiate(notas[random], inipos,Quaternion.identity,transform);
+        Invoke("Destroy", 0.5f);
 	}
-   
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
 }
