927bb61b5d21cb3cd68847590e9749b48b72b7f7
Juan Antonio Naharro Urena <juanitokny11@gmail.com>
2018-12-14 16:02:30 +0100

    Cambios options

--- Assets/Scripts/MyGameSettings.cs
+++ Assets/Scripts/MyGameSettings.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MyGameSettings : MonoBehaviour {
 
 	private static MyGameSettings instance;
-    
-	void Awake (){
+    public Dropdown drop;
+    public bool Fullscreen;
+
+    void Awake (){
 		if (instance == null) {
 			instance = this;
             DontDestroyOnLoad (this);
@@ -15,6 +19,42 @@ public class MyGameSettings : MonoBehaviour {
 	public static MyGameSettings getInstance(){
 		return instance;
 	}
+    public void ChangeLevel()
+    {
+        if (drop.value == 0)
+        {
+            QualitySettings.SetQualityLevel(1);
+        }
+        else if (drop.value == 1)
+        {
+            QualitySettings.SetQualityLevel(2);
+        }
+        else if (drop.value == 2)
+        {
+            QualitySettings.SetQualityLevel(3);
+        }
+        else if (drop.value == 3)
+        {
+            QualitySettings.SetQualityLevel(4);
+        }
 
-	}
+    }
+    public void ChangeResolution()
+    {
+        if (drop.value == 0)
+        {
+            Screen.SetResolution(1280, 720, Fullscreen);
+        }
+        else if (drop.value == 1)
+        {
+            Screen.SetResolution(1600, 1200, Fullscreen);
+        }
+        else if (drop.value == 2)
+        {
+            Screen.SetResolution(1920, 1080, Fullscreen);
+        }
+
+    }
+
+}
 
