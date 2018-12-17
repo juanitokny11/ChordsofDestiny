872f4432139b2d1819364ba8590fa97531c3fea4
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2018-12-17 18:30:16 +0100

    cambios de la pantalla opciones

--- Assets/Scripts/ChangeVolume.cs
+++ Assets/Scripts/ChangeVolume.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class ChangeVolume : MonoBehaviour {
-
+    public Slider generalVolume;
+    
+    public Slider musicVolume;
+    
+    public Slider effectsVolume;
 	public AudioMixer mixer;
+public void Start(){
+    /*PlayerPrefs.GetFloat("volumeG",generalVolume.value);
+    PlayerPrefs.GetFloat("volumeM",musicVolume.value);
+    PlayerPrefs.GetFloat("volumeE",effectsVolume.value);*/
+}
 public void SetLevel(float sliderValue){
     mixer.SetFloat("soundVol",Mathf.Log10(sliderValue)*20);
 }
@@ -17,5 +27,9 @@ public void SetLevel(float sliderValue){
     {
         mixer.SetFloat("EffectsVol", Mathf.Log10(sliderValue) * 20);
     }
-
+public void SaveButtons(){
+    PlayerPrefs.SetFloat("volumeG",generalVolume.value);
+    PlayerPrefs.SetFloat("volumeM",musicVolume.value);
+    PlayerPrefs.SetFloat("volumeE",effectsVolume.value);
+}
 }
