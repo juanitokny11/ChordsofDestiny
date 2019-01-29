3dde8eb6a793cab3889fa126e69d365639f27378
Juan Antonio Naharro Urena <juanitokny11@gmail.com>
2019-01-29 19:11:10 +0100

    no message

--- Assets/Scripts/CameraTest.cs
+++ Assets/Scripts/CameraTest.cs
@@ -6,50 +6,98 @@ public class CameraTest : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform target;
-    public Transform player;
-    public CharacterController controller;
-    public float maxHeight;
-    public float maxPosX;
-    public float maxPosZ;
-    private Vector3 moveDirection = Vector3.zero;
-    public float speed;
-    float minFov = 15f;
-    float maxFov = 90f;
-    float sensitivity = 10f;
+
+    Transform cameraTransform;
+    float zoom;
+    [SerializeField] float minZoom = 2.0f;
+    [SerializeField] float maxZoom = 10.0f;
+
+    [SerializeField] float smoothZoom = 10.0f;
+    [SerializeField] float zoomSensitivity = 5.0f;
+    [SerializeField] float rotSensitivity = 5.0f;
+    [SerializeField] float vertSensitivity = 5.0f;
+
+    float lastZoom;
+    float hitDistance;
+
     void Start()
     {
-        controller = player.GetComponent<CharacterController>();
+        cameraTransform = this.transform;
+        zoom = Mathf.Abs(cameraTransform.position.z - target.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        //moveDirection = transform.TransformDirection(moveDirection).normalized;
-        moveDirection = moveDirection * speed;
-        controller.Move(moveDirection * Time.deltaTime);
-        if (Input.GetMouseButton(0))
-        {
-            target.rotation = Quaternion.Euler(0, Input.mousePosition.x, 0);
-
-            this.transform.position = new Vector3(transform.position.x, Input.mousePosition.y, transform.position.z);
-        }
-        if (this.transform.position.y<=maxHeight && this.transform.position.y>=0)
-        { 
-        transform.LookAt(target);
-        }
-        if (this.transform.position.y >= maxHeight)
-        {
-            this.transform.position = new Vector3(transform.position.x, maxHeight, transform.position.z);
-        }
-        if (this.transform.position.y <= 0)
-        {
-            this.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-        }
-
-        float fov = Camera.main.fieldOfView;
-        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
-        fov = Mathf.Clamp(fov, minFov, maxFov);
-        Camera.main.fieldOfView = fov;
+        Zoom();
+        RotateAround();
+        VerticalMovement();
+        CameraColisionSimple();
     }
+    public void Zoom()
+    {
+        float newZoom = zoom - Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+        newZoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
+
+        zoom = Mathf.Lerp(zoom, newZoom, Time.deltaTime * smoothZoom);
+
+        Vector3 pos = cameraTransform.localPosition;
+        pos.z = -zoom;
+
+        cameraTransform.localPosition = pos;
+    }
+
+    public void RotateAround()
+    {
+        float axisX = Input.GetAxis("Mouse X") * rotSensitivity;
+        //float axisY = Input.GetAxis("Mouse Y") * 5;
+
+        Vector3 rot = target.localEulerAngles;
+
+        rot.y += axisX;
+        //rot.x -= axisY;
+
+        target.localRotation = Quaternion.Euler(rot);
+    }
+
+    public void VerticalMovement()
+    {
+        float axisY = Input.GetAxis("Mouse Y") * vertSensitivity;
+
+        cameraTransform.Translate(Vector3.up * axisY * Time.deltaTime, Space.World);
+
+        Vector3 pos = cameraTransform.localPosition;
+        pos.y = Mathf.Clamp(pos.y, -1, 3);
+        cameraTransform.localPosition = pos;
+
+        Vector3 look = target.position;
+
+        cameraTransform.LookAt(look);
+        
+    }
+    public void CameraColisionSimple()
+    {
+        RaycastHit hit;
+        Vector3 dir = (cameraTransform.position - target.position).normalized;
+        Ray ray = new Ray(target.position, dir);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Debug.Log("HIT");
+            lastZoom = zoom;
+            zoom = hit.distance;
+        }
+        else
+        {
+            //zoom = lastZoom;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!Application.isPlaying) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(target.position, cameraTransform.position - target.position);
+    }
+
 }
