bf473d21a97e8fa8502eb2702703259619b90500
Juan Antonio Naharro Ureña <juanitokny11@gmail.com>
2019-02-16 16:53:26 +0100

    cambios 1

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -46,7 +46,7 @@ public class EnemyBehaviour : MonoBehaviour
     void Start ()
     {
         agent = GetComponent<NavMeshAgent>();
-        //anim =GetComponentInChildren<Animator>();
+        anim =GetComponentInChildren<Animator>();
         attackcollider = GetComponent<BoxCollider>();
         colider = GetComponent<CapsuleCollider>();
        // sound = GetComponentInChildren<SoundPlayer>();
@@ -97,9 +97,9 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Idle()
     {
-        if (timeCounter <= 15.0f){
+        /*if (timeCounter <= 15.0f){
             anim.SetBool("IdleLong", true);
-        }
+        }*/
         if(targetDetected)
         {
             SetChase();
