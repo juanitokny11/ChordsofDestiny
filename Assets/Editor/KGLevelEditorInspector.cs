2a6d5aa42733df1cdfa5b6911a6afdbfdb034920
Juan Antonio Naharro Urena <juanitokny11@gmail.com>
2019-03-27 17:14:14 +0100

    cambio tool de los decals para graffitis etc

---
+++ Assets/Editor/KGLevelEditorInspector.cs
@@ -0,0 +1,76 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace KGUtils
+{    
+    [CustomEditor(typeof(KGLevelEditor))]
+    public class KGLevelEditorInspector : Editor
+    {
+        bool m_CreateObjects=false;
+        bool m_CreateFloor=false;
+        readonly bool m_AddNamesToList=true;
+
+        void OnSceneGUI()
+        {
+            KGLevelEditor l_KGLevelEditor=(KGLevelEditor)target;
+            if(m_CreateObjects)
+            {
+                Selection.activeGameObject=l_KGLevelEditor.gameObject;
+                Event l_Event=Event.current;
+                if(l_Event.isMouse && l_Event.type==EventType.MouseDown && l_Event.button==0)
+                {
+                    RaycastHit l_RaycastHit;
+                    Ray l_MouseRay=HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    if(Physics.Raycast(l_MouseRay, out l_RaycastHit, l_KGLevelEditor.m_DecalDistance, l_KGLevelEditor.m_DecalLayerMask))
+                    {
+                        l_KGLevelEditor.CreateDecal(l_RaycastHit.point, l_RaycastHit.normal, l_RaycastHit.collider.transform);
+                        Undo.RegisterCreatedObjectUndo (l_KGLevelEditor.m_DecalGameObject, "Created Floor");
+                    }
+                }
+            }
+            if(m_CreateFloor)
+            {
+                Event l_Event=Event.current;
+                if(l_Event.type==EventType.KeyDown && l_Event.keyCode==KeyCode.D)
+                {
+                    Plane l_Plane=new Plane(Vector3.up, new Vector3(0.0f, l_KGLevelEditor.m_HeightFloor, 0.0f));
+                    Ray l_MouseRay=HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    float l_Enter=0.0f;
+
+                    if(l_Plane.Raycast(l_MouseRay, out l_Enter))
+                    {
+                        Vector3 l_Hitpoint=l_MouseRay.GetPoint(l_Enter);
+                        l_KGLevelEditor.CreateFloor(l_Hitpoint, l_KGLevelEditor.m_FloorPrefab);
+                        Undo.RegisterCreatedObjectUndo (l_KGLevelEditor.m_FloorInstance, "Created Floor");
+                    }
+                }
+                if(l_Event.type==EventType.KeyDown && l_Event.keyCode==KeyCode.E)
+                {
+                    RaycastHit l_RaycastHit;
+
+                    Ray l_MouseRay=HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                    if(Physics.Raycast(l_MouseRay, out l_RaycastHit, l_KGLevelEditor.m_DecalDistance, l_KGLevelEditor.m_DecalLayerMask))
+                    {
+                        l_KGLevelEditor.DeleteFloor(l_RaycastHit.point,l_RaycastHit.collider.transform.parent.gameObject);
+                        Undo.DestroyObjectImmediate(l_RaycastHit.collider.transform.parent.gameObject);
+                    }
+                }
+            }
+            if(m_AddNamesToList)
+            {
+                Selection.activeGameObject=l_KGLevelEditor.gameObject;
+                l_KGLevelEditor.AddBrushName();
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+            KGLevelEditor l_KGLevelEditor=(KGLevelEditor)target;
+            DrawDefaultInspector();
+            l_KGLevelEditor.m_SelectedBrush=EditorGUILayout.Popup("Selected Brush:", l_KGLevelEditor.m_SelectedBrush, l_KGLevelEditor.m_BrushName.ToArray());
+            if(GUILayout.Button("Create Decal "+ m_CreateObjects))
+                m_CreateObjects=!m_CreateObjects;
+            if(GUILayout.Button("Create Floor "+ m_CreateFloor))
+                m_CreateFloor=!m_CreateFloor;
+        }
+    }
+}
---
+++ Assets/Scripts/Beat Em Up/KGLevelEditor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KGUtils
+{ 
+	public class KGLevelEditor: MonoBehaviour
+	{
+		[Header("Floor")]
+       public List<GameObject> m_Floor;
+		public float m_GridXFloor=2.0f;
+		public float m_GridZFloor=2.0f;
+		public float m_HeightFloor=0.0f;
+		Dictionary<Vector3, GameObject> m_FloorElements;
+		public GameObject m_FloorPrefab;
+        [HideInInspector]public GameObject m_FloorInstance=null;
+		[System.Serializable]
+		public class CDecalBrush
+		{
+			public string m_Name;
+			public List<GameObject> m_DecalPrefabs;
+		}
+		[HideInInspector]public int m_SelectedBrush;
+		[HideInInspector]public List<string> m_BrushName;
+       [HideInInspector]public GameObject m_DecalGameObject;
+		[Header("Decals")]
+		public List<CDecalBrush> m_DecalBrushes;
+		public float m_DecalOffset=0.009f;
+		public LayerMask m_DecalLayerMask=0xffff;
+		public float m_DecalDistance=50.0f;
+		
+		public void AddBrushName()
+		{
+			m_BrushName=new List<string>();
+			foreach(CDecalBrush brush in m_DecalBrushes)
+				m_BrushName.Add(brush.m_Name);
+		}
+		bool AddFloor(Vector3 Position, GameObject Instance)
+		{
+			bool l_AddedFloor=false;
+			if(m_FloorElements==null)
+				m_FloorElements=new Dictionary<Vector3, GameObject>();
+			if(m_FloorElements.ContainsKey(Position))
+			{
+				if(m_FloorElements[Position]==null)
+				{ 
+					m_FloorElements[Position]=Instance;
+					l_AddedFloor=true;
+				}
+			}
+			else
+			{
+				m_FloorElements.Add(Position, Instance);
+				l_AddedFloor=true;
+			}
+			return l_AddedFloor;
+		}
+		bool CanCreateFloor(Vector3 Position)
+		{
+			if(m_FloorElements==null)
+				m_FloorElements=new Dictionary<Vector3, GameObject>();
+			if(m_FloorElements.ContainsKey(Position))
+				return m_FloorElements[Position]==null;
+			return true;
+		}
+		public GameObject CreateFloor(Vector3 Position, GameObject Prefab)
+		{
+			Vector3 l_Position=new Vector3(Position.x-Position.x%m_GridXFloor, m_HeightFloor, Position.z-Position.z%m_GridZFloor);
+			if(CanCreateFloor(l_Position))
+			{ 
+				m_FloorInstance=Instantiate(Prefab,l_Position,Quaternion.identity);
+				AddFloor(l_Position, m_FloorInstance);
+				m_Floor.Add(m_FloorInstance);
+			}
+			//Debug.Log((Position.x-Position.x%m_GridXFloor)+" "+(Position.z-Position.z%m_GridZFloor));
+			return m_FloorInstance;
+		}
+		public void DeleteFloor(Vector3 Position, GameObject Object)
+		{
+			Vector3 l_Position=new Vector3(Position.x-Position.x%m_GridXFloor, m_HeightFloor, Position.z-Position.z%m_GridZFloor);
+			if(!CanCreateFloor(l_Position))
+			{ 
+				DestroyImmediate(Object);
+				m_Floor.Remove(Object);
+			}
+		}
+		public void CreateDecal(Vector3 Position, Vector3 Normal, Transform Parent)
+		{
+			m_DecalGameObject=Instantiate(m_DecalBrushes[m_SelectedBrush].m_DecalPrefabs[UnityEngine.Random.Range(0, m_DecalBrushes[m_SelectedBrush].m_DecalPrefabs.Count)], Position+Normal*m_DecalOffset, Quaternion.LookRotation(Normal), null);
+			m_DecalGameObject.transform.parent=Parent;
+		}
+	}
+}
