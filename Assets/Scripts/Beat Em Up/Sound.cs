35bc056bc8ed006046651976f2639098c1b352c1
ismael1034 <isma1034@gmail.com>
2019-05-02 20:49:44 +0200

    sonido walk groupie implementado y puesto evento de animacion de un debil de aire

--- Assets/Scripts/Beat Em Up/Sound.cs
+++ Assets/Scripts/Beat Em Up/Sound.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Sound : MonoBehaviour
 {
    public AudioSource salidabos;
+   public AudioSource debilaire1;
 
+   public AudioSource walkgroupie;
+ 
    void door()
    {
        salidabos.Play();
+   }
+
+    void airedebil()
+   {
+       debilaire1.Play();
+   }
+
+    void andargroupie()
+   {
+       walkgroupie.pitch = Random.Range(0.7f,1.3f);
+        walkgroupie.volume = Random.Range(0.15f,0.45f);
+       walkgroupie.Play();
+       
    }
 }
